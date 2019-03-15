1efc23b3c18187c3bc9d6860144b27f701e7a6e8
qq123806054 <1665673@gmail.com>
2019-03-15 16:02:43 -0700

    like

--- like.py
+++ like.py
@@ -6,30 +6,27 @@ import reporter
 
 SLEEP_BETWEEN_EACH_LIKE = 20
 
+reporter.set_version("like-ff-2.0")  # set a version tag
 
-def main():
-    reporter.set_version("like-ff-2.0")  # set a version tag
+session = InstaPy(username=insta_username,
+                  password=insta_password,
+                  headless_browser=True,
+                  use_firefox=True,
+                  proxy_address="zproxy.lum-superproxy.io",
+                  proxy_port=22225,
+                  proxy_username="lum-customer-hl_648f8412-zone-static-ip-185.217.61.22",
+                  proxy_password="9y4lv38oag4e")
 
-    session = InstaPy(
-        headless_browser=True,
-        bypass_suspicious_attempt=True,
-        use_firefox=True,
-        **reporter.Arguments().all()  # dump all arguments from command line
-    )
+with smart_run(session):
+    while True:
+        time.sleep(SLEEP_BETWEEN_EACH_LIKE)
+        session.like_by_tags(['love'], amount=1, interact=False)
 
-    with smart_run(session):
-        while True:
-            time.sleep(SLEEP_BETWEEN_EACH_LIKE)
-            session.like_by_tags(['love'], amount=1, interact=False)
+        time.sleep(SLEEP_BETWEEN_EACH_LIKE)
+        session.like_by_tags(['instagood'], amount=1, interact=False)
 
-            time.sleep(SLEEP_BETWEEN_EACH_LIKE)
-            session.like_by_tags(['instagood'], amount=1, interact=False)
+        time.sleep(SLEEP_BETWEEN_EACH_LIKE)
+        session.like_by_tags(['photooftheday'], amount=1, interact=False)
 
-            time.sleep(SLEEP_BETWEEN_EACH_LIKE)
-            session.like_by_tags(['photooftheday'], amount=1, interact=False)
-
-            time.sleep(SLEEP_BETWEEN_EACH_LIKE)
-            session.like_by_tags(['fashion'], amount=1, interact=False)
-
-
-main()
+        time.sleep(SLEEP_BETWEEN_EACH_LIKE)
+        session.like_by_tags(['fashion'], amount=1, interact=False)
