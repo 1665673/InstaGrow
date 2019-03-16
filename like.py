0201fd679df1ae4e79556d0783236c8220525dcc
qq123806054 <1665673@gmail.com>
2019-03-16 15:54:00 -0700

    try

--- like.py
+++ like.py
@@ -6,7 +6,7 @@ import reporter
 
 SLEEP_BETWEEN_EACH_LIKE = 20
 
-reporter.set_version("like-ff-2.0")  # set a version tag
+reporter.set_version("like-ff-2.1-try")  # set a version tag
 
 session = InstaPy(
     bypass_suspicious_attempt=True,
@@ -17,14 +17,18 @@ session = InstaPy(
 
 with smart_run(session):
     while True:
-        time.sleep(SLEEP_BETWEEN_EACH_LIKE)
-        session.like_by_tags(['love'], amount=1, interact=False)
+        try:
+            time.sleep(SLEEP_BETWEEN_EACH_LIKE)
+            session.like_by_tags(['love'], amount=1, interact=False)
 
-        time.sleep(SLEEP_BETWEEN_EACH_LIKE)
-        session.like_by_tags(['instagood'], amount=1, interact=False)
+            time.sleep(SLEEP_BETWEEN_EACH_LIKE)
+            session.like_by_tags(['instagood'], amount=1, interact=False)
 
-        time.sleep(SLEEP_BETWEEN_EACH_LIKE)
-        session.like_by_tags(['photooftheday'], amount=1, interact=False)
+            time.sleep(SLEEP_BETWEEN_EACH_LIKE)
+            session.like_by_tags(['photooftheday'], amount=1, interact=False)
 
-        time.sleep(SLEEP_BETWEEN_EACH_LIKE)
-        session.like_by_tags(['fashion'], amount=1, interact=False)
+            time.sleep(SLEEP_BETWEEN_EACH_LIKE)
+            session.like_by_tags(['fashion'], amount=1, interact=False)
+
+        except Exception as e:
+            reporter.log(e)
