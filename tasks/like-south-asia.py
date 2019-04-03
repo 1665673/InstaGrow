751aec4f1721eb93b2d7b19d7719ba26175e25ff
qq123806054 <1665673@gmail.com>
2019-04-02 19:32:51 -0700

    small bug-fix

---
+++ tasks/like-south-asia.py
@@ -0,0 +1,10 @@
+title = "like by asia locations"
+loop = True
+actions = [
+    {
+        "type": "like-by-locations",
+        "list": ['215567918/kathmandu-nepal/', '278360789/nepal/', '498870164/new-delhi/', '302416621/taiwan/'],
+        "cool-down": 180,
+        "delay-upon-completion": 0
+    }
+]
