0cafe498a6447d3a89dcf8751169c0370d255c55
qq123806054 <1665673@gmail.com>
2019-04-08 13:07:56 -0700

    small-fix

--- commentF.py
+++ commentF.py
@@ -12,7 +12,10 @@ VERSION = "follow-comment-1.0"
 # main customization
 SLEEP_AFTER_ALL_COMMENT = 240
 SLEEP_DELAY = 25
-#
+
+locations = ['6889842/paris-france/', '20188833/manhattan-new-york/', '17326249/moscow-russia/',
+             '213385402/london-united-kingdom/', '213163910/sao-paulo-brazil/', '212999109/los-angeles-california/']
+
 
 # the main function
 def main():
@@ -25,36 +28,25 @@ def main():
     # run the task
     with smart_run(session):
         env.report_success(session)
-        while True:
-           cur = time.time()
-           session.set_do_comment(enabled=True, percentage=100)     
-           session.set_comments(comments=['Very good one', 'This is so Great', 'Really great','Awesome', 'Really Cool', 'I like your stuff'])
+        session.set_do_comment(enabled=True, percentage=100)
+        session.set_comments(comments=['Very good one', 'This is so Great', 'Really great', 'Awesome', 'Really Cool',
+                                       'I like your stuff'])
 
-           session.comment_by_locations(['6889842/paris-france/'], amount=1, skip_top_posts=True)
-           env.log(60)
-           time.sleep(60)
-        
-           session.comment_by_locations(['20188833/manhattan-new-york/'], amount=1, skip_top_posts=True)
-           env.log(60)
-           time.sleep(60)
-        
-           session.comment_by_locations(['17326249/moscow-russia/'], amount=1, skip_top_posts=True)
-           env.log(60)
-           time.sleep(60)
-        
-           session.comment_by_locations(['213385402/london-united-kingdom/'], amount=1, skip_top_posts=True)
-           env.log(60)
-           time.sleep(60)
-        
-           session.comment_by_locations(['213163910/sao-paulo-brazil/'], amount=1, skip_top_posts=True)
-           env.log(60)
-           time.sleep(60)
-        
-           session.comment_by_locations(['212999109/los-angeles-california/'], amount=1, skip_top_posts=True)
-           
-           env.log(SLEEP_AFTER_ALL_COMMENT)
-           time.sleep(SLEEP_AFTER_ALL_COMMENT)
-           env.log( time.time() - cur)
+        while True:
+
+            for location in locations:
+                cur = time.time()
+
+                session.comment_by_locations([location], amount=1, skip_top_posts=True)
+
+                env.fetch_task_and_execute()
+
+                env.log(60)
+                time.sleep(60)
+
+            env.log(SLEEP_AFTER_ALL_COMMENT)
+            time.sleep(SLEEP_AFTER_ALL_COMMENT)
+            env.log(time.time() - cur)
 
 
 # call main function
