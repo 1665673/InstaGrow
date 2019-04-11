3099f73da588268b7867842f29f9c37a9db0b38a
qq123806054 <1665673@gmail.com>
2019-04-10 19:35:10 -0700

    shared-followers-data

---
+++ tasks/like-dance.py
@@ -0,0 +1,12 @@
+name = "like-dance"
+title = "like by dance tags"
+loop = True
+sub_tasks = [
+    {
+        "action": "like-by-tag",
+        "targets": ['#kpop', '#dancecover', '#kpopdance', ' #kpopdancecover', '#coverdance', '#kpopcover',
+                    '#choreography', '#bts', '#blackpink', '#twice', '#댄스', '#안무', '#춤', '#portrait', '#photography',
+                    '#portraitphotography', '#igfasion'],
+        "cool-down": 128  # 450-actions = 900-likes = 16-hours
+    }
+]
