c00e92326522513f21ff20f4b3009188b16a92de
qq123806054 <1665673@gmail.com>
2019-04-12 19:07:04 -0700

    small-fix

--- lib/patch2.py
+++ lib/patch2.py
@@ -7,7 +7,10 @@ def apply():
     sys.modules['instapy'].InstaPy.like_by_locations.__code__ = like_by_locations_patch.__code__
     sys.modules['instapy'].InstaPy.like_by_tags.__code__ = like_by_tags_patch.__code__
     sys.modules['instapy'].InstaPy.comment_by_locations.__code__ = comment_by_locations_patch.__code__
-
+    #
+    sys.modules['instapy.like_util'].check_link.__code__ = check_link_patch.__code__
+    sys.modules['instapy.like_util'].like_image.__code__ = like_image_patch.__code__
+    #
     sys.modules['instapy.unfollow_util'].follow_user.__code__ = follow_user_patch.__code__
     sys.modules['instapy.unfollow_util'].unfollow_user.__code__ = unfollow_user_patch.__code__
     sys.modules['instapy.unfollow_util'].get_following_status.__code__ = get_following_status_patch.__code__
@@ -857,6 +860,252 @@ def comment_by_locations_patch(self,
 #
 #
 #
+#   patched for module like_util.py
+#
+#
+#
+#
+#
+#
+#
+#
+def check_link(browser, post_link, dont_like, mandatory_words,
+               mandatory_language, mandatory_character,
+               is_mandatory_character, check_character_set, ignore_if_contains,
+               logger):
+    """
+    Check the given link if it is appropriate
+
+    :param browser: The selenium webdriver instance
+    :param post_link:
+    :param dont_like: hashtags of inappropriate phrases
+    :param mandatory_words: words of appropriate phrases
+    :param ignore_if_contains:
+    :param logger: the logger instance
+    :return: tuple of
+        boolean: True if inappropriate,
+        string: the username,
+        boolean: True if it is video media,
+        string: the message if inappropriate else 'None',
+        string: set the scope of the return value
+    """
+
+    # Check URL of the webpage, if it already is post's page, then do not
+    # navigate to it again
+    web_address_navigator(browser, post_link)
+
+    """Check if the Post is Valid/Exists"""
+    try:
+        post_page = browser.execute_script(
+            "return window._sharedData.entry_data.PostPage")
+
+    except WebDriverException:  # handle the possible `entry_data` error
+        try:
+            browser.execute_script("location.reload()")
+            update_activity()
+
+            post_page = browser.execute_script(
+                "return window._sharedData.entry_data.PostPage")
+
+        except WebDriverException:
+            post_page = None
+
+    if post_page is None:
+        logger.warning(
+            'Unavailable Page: {}'.format(post_link.encode('utf-8')))
+        return True, None, None, 'Unavailable Page', "Failure"
+
+    """Gets the description of the post's link and checks for the dont_like
+    tags"""
+    graphql = 'graphql' in post_page[0]
+    if graphql:
+        media = post_page[0]['graphql']['shortcode_media']
+        is_video = media['is_video']
+        user_name = media['owner']['username']
+        image_text = media['edge_media_to_caption']['edges']
+        image_text = image_text[0]['node']['text'] if image_text else None
+        location = media['location']
+        location_name = location['name'] if location else None
+        media_edge_string = get_media_edge_comment_string(media)
+        # double {{ allows us to call .format here:
+        owner_comments = browser.execute_script('''
+            latest_comments = window._sharedData.entry_data.PostPage[
+            0].graphql.shortcode_media.{}.edges;
+            if (latest_comments === undefined) {{
+                latest_comments = Array();
+                owner_comments = latest_comments
+                    .filter(item => item.node.owner.username == arguments[0])
+                    .map(item => item.node.text)
+                    .reduce((item, total) => item + '\\n' + total, '');
+                return owner_comments;}}
+            else {{
+                return null;}}
+        '''.format(media_edge_string), user_name)
+
+    else:
+        media = post_page[0]['media']
+        is_video = media['is_video']
+        user_name = media['owner']['username']
+        image_text = media['caption']
+        owner_comments = browser.execute_script('''
+            latest_comments = window._sharedData.entry_data.PostPage[
+            0].media.comments.nodes;
+            if (latest_comments === undefined) {
+                latest_comments = Array();
+                owner_comments = latest_comments
+                    .filter(item => item.user.username == arguments[0])
+                    .map(item => item.text)
+                    .reduce((item, total) => item + '\\n' + total, '');
+                return owner_comments;}
+            else {
+                return null;}
+        ''', user_name)
+
+    if owner_comments == '':
+        owner_comments = None
+
+    """Append owner comments to description as it might contain further tags"""
+    if image_text is None:
+        image_text = owner_comments
+
+    elif owner_comments:
+        image_text = image_text + '\n' + owner_comments
+
+    """If the image still has no description gets the first comment"""
+    if image_text is None:
+        if graphql:
+            media_edge_string = get_media_edge_comment_string(media)
+            image_text = media[media_edge_string]['edges']
+            image_text = image_text[0]['node']['text'] if image_text else None
+
+        else:
+            image_text = media['comments']['nodes']
+            image_text = image_text[0]['text'] if image_text else None
+
+    if image_text is None:
+        image_text = "No description"
+
+    logger.info('Image from: {}'.format(user_name.encode('utf-8')))
+    logger.info('Link: {}'.format(post_link.encode('utf-8')))
+    # logger.info('Description: {}'.format(image_text.encode('utf-8')))
+
+    """Check if mandatory character set, before adding the location to the
+    text"""
+    if mandatory_language:
+        if not check_character_set(image_text):
+            return True, user_name, is_video, 'Mandatory language not ' \
+                                              'fulfilled', "Not mandatory " \
+                                                           "language"
+
+    """Append location to image_text so we can search through both in one
+    go."""
+    if location_name:
+        # logger.info('Location: {}'.format(location_name.encode('utf-8')))
+        image_text = image_text + '\n' + location_name
+
+    if mandatory_words:
+        if not any((word in image_text for word in mandatory_words)):
+            return True, user_name, is_video, 'Mandatory words not ' \
+                                              'fulfilled', "Not mandatory " \
+                                                           "likes"
+
+    image_text_lower = [x.lower() for x in image_text]
+    ignore_if_contains_lower = [x.lower() for x in ignore_if_contains]
+    if any((word in image_text_lower for word in ignore_if_contains_lower)):
+        return False, user_name, is_video, 'None', "Pass"
+
+    dont_like_regex = []
+
+    for dont_likes in dont_like:
+        if dont_likes.startswith("#"):
+            dont_like_regex.append(dont_likes + "([^\d\w]|$)")
+        elif dont_likes.startswith("["):
+            dont_like_regex.append("#" + dont_likes[1:] + "[\d\w]+([^\d\w]|$)")
+        elif dont_likes.startswith("]"):
+            dont_like_regex.append("#[\d\w]+" + dont_likes[1:] + "([^\d\w]|$)")
+        else:
+            dont_like_regex.append(
+                "#[\d\w]*" + dont_likes + "[\d\w]*([^\d\w]|$)")
+
+    for dont_likes_regex in dont_like_regex:
+        quash = re.search(dont_likes_regex, image_text, re.IGNORECASE)
+        if quash:
+            quashed = \
+                (((quash.group(0)).split('#')[1]).split(' ')[0]).split('\n')[
+                    0].encode(
+                    'utf-8')  # dismiss possible space and newlines
+            iffy = ((re.split(r'\W+', dont_likes_regex))[
+                        3] if dont_likes_regex.endswith(
+                '*([^\\d\\w]|$)') else  # 'word' without format
+                    (re.split(r'\W+', dont_likes_regex))[
+                        1] if dont_likes_regex.endswith(
+                        '+([^\\d\\w]|$)') else  # '[word'
+                    (re.split(r'\W+', dont_likes_regex))[
+                        3] if dont_likes_regex.startswith(
+                        '#[\\d\\w]+') else  # ']word'
+                    (re.split(r'\W+', dont_likes_regex))[1])  # '#word'
+            inapp_unit = 'Inappropriate! ~ contains "{}"'.format(
+                quashed if iffy == quashed else
+                '" in "'.join([str(iffy), str(quashed)]))
+            return True, user_name, is_video, inapp_unit, "Undesired word"
+
+    return False, user_name, is_video, 'None', "Success"
+
+
+def like_image_patch(browser, username, blacklist, logger, logfolder):
+    """Likes the browser opened image"""
+    # check action availability
+    if quota_supervisor("likes") == "jump":
+        return False, "jumped"
+
+    like_xpath = "//section/span/button/span[@aria-label='Like']"
+    unlike_xpath = "//section/span/button/span[@aria-label='Unlike']"
+
+    # find first for like element
+    like_elem = browser.find_elements_by_xpath(like_xpath)
+
+    if len(like_elem) == 1:
+        # sleep real quick right before clicking the element
+        sleep(2)
+        click_element(browser, like_elem[0])
+        # check now we have unlike instead of like
+        liked_elem = browser.find_elements_by_xpath(unlike_xpath)
+
+        if len(liked_elem) == 1:
+            # logger.info('--> Image Liked!')
+            update_activity('likes')
+
+            if blacklist['enabled'] is True:
+                action = 'liked'
+                add_user_to_blacklist(
+                    username, blacklist['campaign'], action, logger, logfolder)
+
+            # get the post-like delay time to sleep
+            naply = get_action_delay("like")
+            sleep(naply)
+            return True, "success"
+
+        else:
+            # if like not seceded wait for 2 min
+            logger.info('--> Image was not able to get Liked! maybe blocked ?')
+            sleep(120)
+
+    else:
+        liked_elem = browser.find_elements_by_xpath(unlike_xpath)
+        if len(liked_elem) == 1:
+            # logger.info('--> Image already liked!')
+            return False, "already liked"
+
+    logger.info('--> Invalid Like Element!')
+
+    return False, "invalid element"
+
+
+#
+#
+#
+#
+#
 #
 #   patches for
 #   sys.modules['instapy.unfollow_util']
@@ -872,7 +1121,7 @@ def follow_user_patch(browser, track, login, user_name, button, blacklist, logge
     # go to user's main profile page
     user_link = "https://www.instagram.com/{}/".format(user_name)
     web_address_navigator(browser, user_link)
-    logger.info(user_link)
+    logger.info("Arrived: {}".format(user_link))
     # stay 1 second in profile page
     sleep(1)
     #
@@ -1003,7 +1252,7 @@ def unfollow_user_patch(browser, track, username, person, person_id, button, rel
     # go to user's main profile page
     user_link = "https://www.instagram.com/{}/".format(person)
     web_address_navigator(browser, user_link)
-    logger.info(user_link)
+    logger.info("Arrived: {}".format(user_link))
     # stay 1 second in profile page
     sleep(1)
     #
