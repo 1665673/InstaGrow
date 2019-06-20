d123bcd221eae0fc9584427c6ae557c4ac43112d
qq123806054 <1665673@gmail.com>
2019-06-20 14:41:07 -0700

    add-one-custom-task

---
+++ tasks/like-240-cece.py
@@ -0,0 +1,10 @@
+name = "like"
+title = "like by tags"
+loop = True
+sub_tasks = [
+    {
+        "action": "like-by-tag",
+        "targets": ['cardib', 'megthestallion', 'sawettıe', 'quavo', 'bhadbhabie', 'yungthug', 'nikiminaj'],
+        "cool-down": 240  # 240-likes = 16-hours
+    }
+]
