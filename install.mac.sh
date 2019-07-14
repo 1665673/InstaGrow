588ebcc7897ab9ecc3cd2dbb91ec00596f017f90
qq123806054 <1665673@gmail.com>
2019-07-14 16:35:43 -0700

    gui chrome

--- install.mac.sh
+++ install.mac.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 cp firefox/geckodriver.mac /usr/local/bin/geckodriver
+cp chrome/chromedriver.mac64 /usr/local/bin/chromedriver
+
 pip3 install -r requirements.txt
--- install.sh
+++ install.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
+# verify custom swap file size
 SWAP=7G
 if [ $# -gt 0 ];then
     SWAP=$1
 fi
 
+# update apt source
 apt-get -y update
 apt-get -y install python3-pip
 
 # headless X-11
 apt-get -y install xvfb
 
+# install firefox of specific version 66.0
 #apt-get -y install firefox
 apt-get -y install firefox
 apt-get -y remove firefox
@@ -25,11 +28,20 @@ cp -r ./firefox/firefox /usr/lib/firefox
 ln -s /usr/lib/firefox/firefox /usr/bin/firefox
 apt-mark hold firefox
 
-cp firefox/geckodriver.ubuntu /usr/local/bin/geckodriver
-cp chrome/chromedriver_linux64 /usr/local/bin/chromedriver
+# install chrome of specific version 75.0
+if test ! -f ./google-chrome-stable_75.0.3770.100-1_amd64.deb; then
+    wget http://dl.google.com/linux/deb/pool/main/g/google-chrome-stable/google-chrome-stable_75.0.3770.100-1_amd64.deb
+    sudo dpkg -i google-chrome-stable_75.0.3770.100-1_amd64.deb
+fi
 
+# copy firefox & chrome drivers
+cp firefox/geckodriver.ubuntu /usr/local/bin/geckodriver
+cp chrome/chromedriver.linux64 /usr/local/bin/chromedriver
+
+# install InstaGrow dependencies
 pip3 install -r requirements.txt
 
+# allocate swap file
 fallocate -l ${SWAP} /swapfile
 chmod 600 /swapfile
 mkswap /swapfile
@@ -37,6 +49,7 @@ swapon /swapfile
 Xvfb :99 &
 #export DISPLAY=:99
 
+# create boot list
 if test ! -f /etc/rc.local; then
     # echo "rc.local1 does not exists"
     echo -e '#!/bin/bash\n\nexit 0' >> /etc/rc.local
@@ -46,6 +59,7 @@ chown root:root /etc/rc.local
 chmod 755 /etc/rc.local
 systemctl enable rc-local.service
 
+# update boot list (part 1, for droplet.py service)
 if ! grep -q "droplet.py" /etc/rc.local; then
     dir=$(echo $PWD | sed 's/\//\\\//g')
     #sed -i "s/^exit 0/swapon \/swapfile\ncd $dir\npython3 droplet.py\nexit 0/" /etc/rc.local
@@ -56,11 +70,13 @@ if ! grep -q "droplet.py" /etc/rc.local; then
     sed -i "s/^exit 0/screen -d -m python3 droplet.py\nexit 0/" /etc/rc.local
 fi
 
+# update boot list (part 2, for Xvfb service)
 if ! grep -q "Xvfb" /etc/rc.local; then
     sed -i "s/^exit 0/Xvfb :99 \&\nexit 0/" /etc/rc.local
     #sed -i "s/^exit 0/export DISPLAY=:99\nexit 0/" /etc/rc.local
 fi
 
+# update bash environment profile
 if ! grep -q "DISPLAY" /etc/profile; then
     sed -i '$s/$/\nexport DISPLAY=:99/' /etc/profile
 fi
